368f9f6312c687dd914052443be720b9ab1b0721
brigitteorina <brigitteorina@gmail.com>
2023-06-23 20:14:45 +0300

    complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,24 +2,20 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
-
   def create
     author = Author.create(author_params)
-    if author.save
-
-    render json: author, status: :created
-    else 
-      render json:{errors: author.errors.full_messages},status: :unprocessable_entity
+    if author.valid?
+      render json: author, status: :created
+    else
+      render json: {errors: author.errors}, status: :unprocessable_entity
     end
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
 end
